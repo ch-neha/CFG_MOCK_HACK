b42dd236e48de2ea97cc7ac5e0e403e9c9b6e85c
KYasaswini <yasaswini7302@gmail.com>
2022-05-30 14:30:38 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {NavLink} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const App=()=>
+{
+  return(
+    <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+    <a className="navbar-brand" href="#">My App</a>
+
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <NavLink className="nav-link" to=''>Home</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to=''>Login</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to=''>Sign Up</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to=''>Content</NavLink>
+        </li>
+      </ul>
     </div>
-  );
+  </div>
+</nav>
+</div>
+
+  )
 }
 
 export default App;
